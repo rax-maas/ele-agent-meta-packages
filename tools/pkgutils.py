e4e3f29f2c10b9c0abfce8ae5859921a40bbfdc3
richarxt <richa.garg@rackspace.com>
2022-02-17 22:42:47 +0530

    added check to read form redhat-release file always and additional check for rocky

--- tools/pkgutils.py
+++ tools/pkgutils.py
@@ -11,7 +11,7 @@ import subprocess
 # TODO: Windows does MSI?
 
 deb = ['debian', 'ubuntu']
-rpm = ['redhat', 'fedora', 'suse', 'opensuse', 'centos']
+rpm = ['redhat', 'fedora', 'suse', 'opensuse', 'centos', 'rocky', 'almalinux']
 
 dist = platform.dist()[0].lower()
 
@@ -77,13 +77,12 @@ def pkg_dir():
             major = dist[1].split(".")[0]
             distro = dist[0]
 
-            # http://bugs.centos.org/view.php?id=5197
-            # CentOS 5.7 identifies as redhat
-            if int(major) <= 5 and distro == "redhat":
-                f = open('/etc/redhat-release')
-                new_dist = f.read().lower().split(" ")[0]
-                if new_dist == "centos":
-                    distro = "centos"
+            f = open('/etc/redhat-release')
+            new_dist = f.read().lower().split(" ")[0]
+            if new_dist == "rocky":
+              distro = "rockylinux"
+    	    else:
+	      distro = new_dist
 
             dist = (distro, major)
 
