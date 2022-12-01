b70a3f394ea0a26f60858e8e1538077524051e8f
richarxt <richagarg76@gmail.com>
2022-12-01 14:59:27 +0530

    added github worklow and made some code quality changes in version file

--- tools/get_version.py
+++ tools/get_version.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
-
+""" Module returning the meta version using config file """
 import json
 
 if __name__ == "__main__":
 
-
     # Opening JSON file
-    f = open('config.json')
+    CONFIG_FILE = open('config.json')
 
     # returns JSON object as a dictionary
-    js = json.load(f)
+    JS_DICT = json.load(CONFIG_FILE)
 
-    print(js['meta_version'])
+    print JS_DICT['meta_version']
+
     # close file
-    f.close()
+    CONFIG_FILE.close()
