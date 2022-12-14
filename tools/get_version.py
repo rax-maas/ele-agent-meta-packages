0551720aaff46dfe9e38db64b83a3af194aeb38c
rohitsngh27 <rohitsngh27@gmail.com>
2022-12-14 22:05:01 +0530

    Update location for config file and changes in Makefile 1. Updated the location for config.json file in get_version.py 2. Pylint warning fixes made the constant in capital case which need to be also fixed in Makefile too.

--- tools/get_version.py
+++ tools/get_version.py
@@ -5,7 +5,7 @@ import json
 if __name__ == "__main__":
 
     # Opening JSON file
-    CONFIG_FILE = open('config.json')
+    CONFIG_FILE = open('tools/config.json')
 
     # returns JSON object as a dictionary
     JS_DICT = json.load(CONFIG_FILE)
