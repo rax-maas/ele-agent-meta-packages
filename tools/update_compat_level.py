5d9e545084780d2a82e01b85dfc341d52aa2a686
rohitsngh27 <rohitsngh27@gmail.com>
2022-12-14 20:26:27 +0530

    Add changes to update control file We need to ensure that correct build-depends property is set as per compat-level. Added default Build-Depends property in all of the control files and updated the script to change property dynamically.

--- tools/update_compat_level.py
+++ tools/update_compat_level.py
@@ -1,14 +1,17 @@
-"""Updates the compat level by running shell command"""
-
+"""Updates the compat level and control file by running shell command"""
 import os
 import sys
 
-def update_compat_level():
-    """Updates the compat level to the value provided in argument"""
+def update_compat_level_and_control_file():
+    """Updates the compat level to the value provided in argument and also updates Build Depends property for control file"""
     compat_level = sys.argv[1]
-    command_to_execute = "for d in DEB/*/ ; do (cd ""$d"" && echo "+ \
+    update_compat_level_command = "for d in DEB/*/ ; do (cd ""$d"" && echo "+ \
         compat_level+" > debian/compat); done"
-    os.system(command_to_execute)
+    build_depends = "debhelper (>= " + compat_level + "~)"
+    update_control_file = "for d in DEB/*/ ; do (cd ""$d"" && sed -i 's/debhelper (>= 5~)/" + \
+        build_depends + "/g' debian/control); done"
+    os.system(update_compat_level_command)
+    os.system(update_control_file)
 
 if __name__ == '__main__':
-    update_compat_level()
+    update_compat_level_and_control_file()
