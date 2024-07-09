5480fdf95987c7e450ee679c0d029922287f067f
richarxt <richagarg76@gmail.com>
2024-07-09 15:00:34 +0530

    Revert "Fix errors after pyhton3 compatible changes"
    This reverts commit ed2b8ad904560219911fbd7177d6392cabd06104.

--- tools/get_platform.py
+++ tools/get_platform.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 """Module to Get platform information """
-# pylint: disable=import-error
-from tools.pkgutils import pkg_dir
+from .pkgutils import pkg_dir
 
 print(pkg_dir())
